8a947f68068dd935bd5afe5e2b5a486a04651d19
yoenai <yguerrerod@student.rcc.edu>
2015-01-06 20:03:06 -0800

    Signed-off-by: yoenai <yguerrerod@student.rcc.edu>

---
+++ Class_Lab/Hello world/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: Yoenai Guerrero
+ * Created on January 6, 2015, 6:05 PM
+ * Purpose: Our first program 
+ */
+//System libraries
+#include <iostream>
+
+// user Libraries
+
+// Global Constants 
+
+//Function Prototypes 
+
+// Execution Begins Here!
+
+using namespace std;
+
+int main(int argc, char** argv) {
+//Output Hello World 
+    cout<<"Hello World"<<endl;
+    //Exit state 
+    return 0;
+}
+
