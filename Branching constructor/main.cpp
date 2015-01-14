870aa4b7bbbff60c58570bc4290ed54ac4430887
yoenai <yoenaig@gmail.com>
2015-01-13 21:31:56 -0800

    Signed-off-by: yoenai <yoenaig@gmail.com>

---
+++ Branching constructor/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author:Yoeani Guerrero
+ * Created on January 13, 2015, 9:16 PM
+ * Purpose: Illustrate branching code
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include<ctime>
+using namespace std;
+//Randomly initialize a variable 
+int main(int argc, char** argv) {
+    srand(static_cast< unsigned int>(time(0)) );
+    bool torf=rand()%2;
+    //illustratre the ternary operator
+    cout<<"The random boolran us";
+    cout<<()
+    return 0;
+}
+
---
+++ Homework/Assigment2/Savitch_9thEd_chap2_ProgProj3/main.cpp
@@ -0,0 +1,41 @@
+/* 
+ * File:   main.cpp
+ * Author:Yoenai Guerrero
+ * Created on January 13, 2015, 8:17 PM
+ */
+
+#include <cstdlib>
+#include<iostream>
+using namespace std;
+//User Libraries 
+
+//Global Constants 
+const int CNVHRMN=60;// Conversion from Hrs to Mins 
+const int CNVMNSC=60;// FInish this 
+
+int main(int argc, char** argv) {
+//Declare and initialize
+   float mph;//Miles per hour on a treadmi
+   float hpm;//Pace as hours per mile 
+   float fmpm;//Miles per minute as float
+   int impm;// Miles per minute as an integer
+   float fmspm;//Fraction left for seconds per minutes 
+   //Prompt for the input
+   cout<<"How many miles are you running"<<endl;
+   cout<<"in an hour"<<;
+   cout<<"Format answer as a float."<<endl;
+   cin>> mph;
+   //Calculate the relavent pace
+   //calculate the relavent pace 
+   hpm=1/mph;
+   fmpm=hpm*CNVHRMN;
+   fmspm=hpm*CNVHRMN;
+   impm=static_cast<int>(fmpm);
+   fmspm=(fmom-)
+           
+           
+   
+    
+    return 0;
+}
+
